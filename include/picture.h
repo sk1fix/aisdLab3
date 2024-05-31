d23839aefd6b88d1f176f055b38609b913e5833d
sk1fix <agafonovtoha427@gmail.com>
2024-05-31 20:36:43 +0400

    vertex

--- include/picture.h
+++ include/picture.h
@@ -13,7 +13,7 @@ class Graph {
 private:
     struct Vertex {
         int _id;
-        V value;
+        Ver value;
         vector<Edge> edges;
     };
     struct Edge {
@@ -21,4 +21,34 @@ private:
         int _id;
     };
     vector<Vertex> graph;
+public:
+    bool has_vertex(int id) const {
+        return find_if(graph.begin(), graph.end(), [id](const Vertex& v) { return v._id == id; }) != graph.end();
+    }
+    void add_vertex(int id, const Ver& value) {
+        if (!has_vertex(id)) {
+            graph.push_back(Vertex{ id, value, {} });
+        }
+    }
+    bool remove_vertex(int id) {
+        auto temp = find_if(graph.begin(), graph.end(),
+            [id](const Vertex& v) { return v._id == id; });
+        if (temp == graph.end()) return false;
+
+        graph.erase(it);
+        for (auto& vertex : graph) {
+            vertex.edges.erase(
+                remove_if(vertex.edges.begin(), vertex.edges.end(),
+                    [id](const Edge& e) { return e._id == id; }),
+                vertex.edges.end());
+        }
+        return true;
+    }
+    vector<int> vertices() const {
+        vector<int> result;
+        for (const auto& vertex : graph) {
+            result.push_back(vertex._id);
+        }
+        return result;
+    }
 };
