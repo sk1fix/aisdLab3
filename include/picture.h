8ff3692a6b4e977f00d2bd62392eb6d10770c8d6
sk1fix <agafonovtoha427@gmail.com>
2024-06-04 14:38:13 +0400

    /

--- include/picture.h
+++ include/picture.h
@@ -7,36 +7,39 @@
 #include <limits>
 #include <functional>
 #include <queue>
+
 using namespace std;
-template<typename Ver, typename Distance = double>
+
+template<typename Ver, typename Distance = double, typename V>
 class Graph {
 private:
-    struct Vertex {
-        int _id;
-        Ver value;
-        vector<Edge> edges;
-    };
     struct Edge {
         Distance distance;
-        int _id;
+        V _id; 
     };
-    vector<Vertex> graph;
+
+    struct Vertex {
+        V _id; 
+        Ver value;
+        vector<Edge> edges; 
+    };
+
+    unordered_map<V, Vertex> graph;
+
 public:
-    bool has_vertex(int id) const {
-        return find_if(graph.begin(), graph.end(), [id](const Vertex& v) { return v._id == id; }) != graph.end();
+    bool has_vertex(V id) const {
+        return graph.find(id) != graph.end();
     }
-    void add_vertex(int id, const Ver& value) {
+    void add_vertex(V id, const Ver& value) {
         if (!has_vertex(id)) {
-            graph.push_back(Vertex{ id, value, {} });
+            graph[id] = Vertex{ id, value, {} };
         }
     }
-    bool remove_vertex(int id) {
-        auto temp = find_if(graph.begin(), graph.end(),
-            [id](const Vertex& v) { return v._id == id; });
-        if (temp == graph.end()) return false;
-
+    bool remove_vertex(V id) {
+        auto it = graph.find(id);
+        if (it == graph.end()) return false;
         graph.erase(it);
-        for (auto& vertex : graph) {
+        for (auto& [_, vertex] : graph) {
             vertex.edges.erase(
                 remove_if(vertex.edges.begin(), vertex.edges.end(),
                     [id](const Edge& e) { return e._id == id; }),
@@ -44,26 +47,23 @@ public:
         }
         return true;
     }
-    vector<int> vertices() const {
-        vector<int> result;
-        for (const auto& vertex : graph) {
-            result.push_back(vertex._id);
+    vector<V> vertices() const {
+        vector<V> result;
+        for (const auto& [id, _] : graph) {
+            result.push_back(id);
         }
         return result;
     }
-    void add_edge(int from_id, int to_id, const Distance& d) {
-        auto temp = find_if(graph.begin(), graph.end(),
-            [from_id](const Vertex& v) { return v._id == from_id; });
-        if (temp != graph.end()) {
-            temp->edges.push_back(Edge{ d, to_id });
+    void add_edge(V from_id, V to_id, const Distance& d) {
+        if (has_vertex(from_id) && has_vertex(to_id)) {
+            graph[from_id].edges.push_back(Edge{ d, to_id });
         }
     }
-    bool remove_edge(int from_id, int to_id) {
-        auto temp = find_if(graph.begin(), graph.end(),
-            [from_id](const Vertex& v) { return v._id == from_id; });
-        if (temp == graph.end()) return false;
+    bool remove_edge(V from_id, V to_id) {
+        auto it = graph.find(from_id);
+        if (it == graph.end()) return false;
 
-        auto& edges = temp->edges;
+        auto& edges = it->second.edges;
         auto edge_it = remove_if(edges.begin(), edges.end(),
             [to_id](const Edge& e) { return e._id == to_id; });
         if (edge_it == edges.end()) return false;
@@ -71,19 +71,18 @@ public:
         edges.erase(edge_it, edges.end());
         return true;
     }
-    bool remove_edge(const Edge& e, int from_id) {
+    bool remove_edge(const Edge& e, V from_id) {
         return remove_edge(from_id, e._id);
     }
-    bool has_edge(int from_id, int to_id) const {
-        auto temp = find_if(graph.begin(), graph.end(),
-            [from_id](const Vertex& v) { return v._id == from_id; });
-        if (temp == graph.end()) return false;
+    bool has_edge(V from_id, V to_id) const {
+        auto it = graph.find(from_id);
+        if (it == graph.end()) return false;
 
-        const auto& edges = temp->edges;
+        const auto& edges = it->second.edges;
         return any_of(edges.begin(), edges.end(),
             [to_id](const Edge& e) { return e._id == to_id; });
     }
-    bool has_edge(const Edge& e, int from_id) const {
+    bool has_edge(const Edge& e, V from_id) const {
         return has_edge(from_id, e._id);
     }
 };
