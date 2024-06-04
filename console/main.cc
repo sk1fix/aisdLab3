a95608961c524e70fb393109a6200d622d2d67d0
sk1fix <agafonovtoha427@gmail.com>
2024-06-04 16:54:16 +0400

    mb final

--- console/main.cc
+++ console/main.cc
@@ -1,61 +1,39 @@
 #pragma once
 #include<picture.h>
-using namespace tree;
+using namespace graph;
 int main()
 {
-	vector<int> a = { 3, 2, 2, 4 };
-	vector<int>new_a = findDuplicates<int>(a);
-	for (int i = 0; i < size(new_a); i++) {
-		cout << new_a[i] << endl;
-	}
-	/*BinarySearchTree<int> tree;
-	vector<int>new_a = findDuplicates<int>(a, tree);*/
+    graph::Graph<string> g;
+    g.add_vertex(1, "A");
+    g.add_vertex(2, "B");
+    g.add_vertex(3, "C");
+    g.add_edge(1, 2, 1.0);
+    g.add_edge(2, 3, 2.0);
+    g.add_edge(1, 3, 4.0);
 
-	BinarySearchTree<int> bst;
-	bst.insert(3);
-	bst.insert(45);
-	bst.insert(8);
-	bst.insert(4);
-	bst.insert(47);
-	bst.insert(87);
-	bst.print();
-	bst.erase(47);
-	bst.print();
-	//bst.fillTreeWithRandomNumbers(1000);
-	//Time taken to fill the tree with 1000 random numbers: 0.000800439 seconds
-	//Time taken to fill the vec with 1000 random numbers : 0.000129292 seconds
-	//bst.fillTreeWithRandomNumbers(10000);
-	//Time taken to fill the tree with 10000 random numbers: 0.00919021 seconds
-	//Time taken to fill the vec with 10000 random numbers : 0.00108887 seconds
-	//bst.fillTreeWithRandomNumbers(100000);
-	//Time taken to fill the tree with 100000 random numbers: 0.0797373 seconds
-	//Time taken to fill the vec with 100000 random numbers : 0.0109607 seconds
-	//bst.containsTreeWithRandomNumbers(1000);
-	//Time taken to contains the tree with 1000 random numbers: 1.364e-07 seconds
-	//Time taken to contains the vec with 1000 random numbers : 0.000142592 seconds
-	//bst.containsTreeWithRandomNumbers(10000);
-	//Time taken to contains the tree with 10000 random numbers: 2.696e-07 seconds
-	//Time taken to contains the vec with 10000 random numbers : 0.00143512 seconds
-	//bst.containsTreeWithRandomNumbers(100000);
-	//Time taken to contains the tree with 100000 random numbers: 5.04e-07 seconds
-	//Time taken to contains the vec with 100000 random numbers : 0.00971749 seconds
-	//bst.insertTreeWithRandomNumbers(1000);
-	//Time taken to insert the tree with 1000 random numbers: 3.287e-07 seconds
-	//Time taken to insert the vec with 1000 random numbers : 1.508e-07 seconds
-	//bst.insertTreeWithRandomNumbers(10000);
-	//Time taken to insert the tree with 10000 random numbers: 8.19e-07 seconds
-	//Time taken to insert the vec with 10000 random numbers : 3.84e-07 seconds
-	//bst.insertTreeWithRandomNumbers(100000);
-	//Time taken to insert the tree with 100000 random numbers: 8.853e-07 seconds
-	//Time taken to insert the vec with 100000 random numbers : 2.313e-07 seconds
-	//bst.eraseTreeWithRandomNumbers(1000);
-	//Time taken to erase the tree with 1000 random numbers: 2.033e-07 seconds
-	//Time taken to erase the vec with 1000 random numbers : 4.1019e-06 seconds
-	//bst.eraseTreeWithRandomNumbers(10000);
-	//Time taken to erase the tree with 10000 random numbers: 7.865e-07 seconds
-	//Time taken to erase the vec with 10000 random numbers : 4.77749e-05 seconds
-	//bst.eraseTreeWithRandomNumbers(100000);
-	//Time taken to erase the tree with 100000 random numbers: 7.124e-07 seconds
-	//Time taken to erase the vec with 100000 random numbers : 0.000266338 seconds
-	return 0;
+    auto vertices = g.vertices();
+    cout << "Vertices: ";
+    for (const auto& v : vertices) {
+        cout << v << " ";
+    }
+    cout << endl;
+
+    auto edges = g.edges(1);
+    cout << "Edges from vertex 1: ";
+    for (const auto& e : edges) {
+        cout << "(" << e.to << ", " << e.distance << ") ";
+    }
+    cout << endl;
+
+    auto path = g.shortest_path(1, 3);
+    cout << "Shortest path from 1 to 3: ";
+    for (const auto& e : path) {
+        cout << "(" << e.to << ", " << e.distance << ") ";
+    }
+    cout << endl;
+
+    int optimal_store_id = g.optimal_store();
+    cout << "Optimal store location: " << optimal_store_id << endl;
+
+    return 0;
 }
--- include/picture.h
+++ include/picture.h
@@ -2,7 +2,6 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-#include <memory>
 #include <algorithm>
 #include <limits>
 #include <functional>
@@ -10,79 +9,175 @@
 
 using namespace std;
 
-template<typename Ver, typename Distance = double, typename V>
-class Graph {
-private:
-    struct Edge {
-        Distance distance;
-        V _id; 
+namespace graph {
+
+    template<typename Ver, typename Distance = double>
+    class Graph {
+    private:
+        struct Edge {
+            Distance distance;
+            int to; 
+        };
+        struct Vertex {
+            Ver value; 
+            vector<Edge> edges; 
+        };
+        unordered_map<int, Vertex> graph;
+    public:
+        bool has_vertex(int id) const {
+            return graph.find(id) != graph.end();
+        }
+        void add_vertex(int id, const Ver& value) {
+            if (!has_vertex(id)) {
+                graph[id] = Vertex{ value, {} };
+            }
+        }
+        bool remove_vertex(int id) {
+            auto it = graph.find(id);
+            if (it == graph.end()) return false;
+            graph.erase(it);
+            for (auto& [_, vertex] : graph) {
+                vertex.edges.erase(
+                    remove_if(vertex.edges.begin(), vertex.edges.end(),
+                        [id](const Edge& e) { return e.to == id; }),
+                    vertex.edges.end());
+            }
+            return true;
+        }
+        vector<int> vertices() const {
+            vector<int> result;
+            for (const auto& [id, _] : graph) {
+                result.push_back(id);
+            }
+            return result;
+        }
+        void add_edge(int from_id, int to_id, const Distance& d) {
+            if (has_vertex(from_id) && has_vertex(to_id)) {
+                graph[from_id].edges.push_back(Edge{ d, to_id });
+            }
+        }
+        bool remove_edge(int from_id, int to_id) {
+            auto it = graph.find(from_id);
+            if (it == graph.end()) return false;
+
+            auto& edges = it->second.edges;
+            auto edge_it = remove_if(edges.begin(), edges.end(),
+                [to_id](const Edge& e) { return e.to == to_id; });
+            if (edge_it == edges.end()) return false;
+
+            edges.erase(edge_it, edges.end());
+            return true;
+        }
+        bool remove_edge(const Edge& e, int from_id) {
+            return remove_edge(from_id, e.to);
+        }
+        bool has_edge(int from_id, int to_id) const {
+            auto it = graph.find(from_id);
+            if (it == graph.end()) return false;
+            const auto& edges = it->second.edges;
+            return any_of(edges.begin(), edges.end(),
+                [to_id](const Edge& e) { return e.to == to_id; });
+        }
+        bool has_edge(const Edge& e, int from_id) const {
+            return has_edge(from_id, e.to);
+        }
+        vector<Edge> edges(int vertex_id) const {
+            auto it = graph.find(vertex_id);
+            if (it != graph.end()) {
+                return it->second.edges;
+            }
+            return {};
+        }
+        size_t order() const {
+            return graph.size();
+        }
+        size_t degree(int vertex_id) const {
+            auto it = graph.find(vertex_id);
+            if (it != graph.end()) {
+                return it->second.edges.size();
+            }
+            return 0;
+        }
+        vector<Edge> shortest_path(int from_id, int to_id) const {
+            unordered_map<int, Distance> distances;
+            unordered_map<int, int> predecessors;
+            for (const auto& [id, _] : graph) {
+                distances[id] = numeric_limits<Distance>::infinity();
+            }
+            distances[from_id] = 0;
+
+            for (size_t i = 0; i < graph.size() - 1; ++i) {
+                for (const auto& [id, vertex] : graph) {
+                    for (const auto& edge : vertex.edges) {
+                        if (distances[id] + edge.distance < distances[edge.to]) {
+                            distances[edge.to] = distances[id] + edge.distance;
+                            predecessors[edge.to] = id;
+                        }
+                    }
+                }
+            }
+            for (const auto& [id, vertex] : graph) {
+                for (const auto& edge : vertex.edges) {
+                    if (distances[id] + edge.distance < distances[edge.to]) {
+                        throw runtime_error("Graph contains a negative-weight cycle");
+                    }
+                }
+            }
+            vector<Edge> path;
+            for (int v = to_id; v != from_id; v = predecessors[v]) {
+                if (predecessors.find(v) == predecessors.end()) {
+                    throw runtime_error("No path found");
+                }
+                int u = predecessors[v];
+                auto it = find_if(graph.at(u).edges.begin(), graph.at(u).edges.end(),
+                    [v](const Edge& e) { return e.to == v; });
+                if (it != graph.at(u).edges.end()) {
+                    path.push_back(*it);
+                }
+            }
+            reverse(path.begin(), path.end());
+            return path;
+        }
+        vector<int> walk(int start_id) const {
+            vector<int> result;
+            unordered_set<int> visited;
+            function<void(int)> dfs = [&](int id) {
+                if (visited.count(id)) return;
+                visited.insert(id);
+                result.push_back(id);
+                for (const auto& edge : graph.at(id).edges) {
+                    dfs(edge.to);
+                }
+            };
+            dfs(start_id);
+            return result;
+        }
+        int optimal_store() const {
+            int best_id = -1;
+            Distance min_max_distance = numeric_limits<Distance>::infinity();
+            for (const auto& [id, _] : graph) {
+                Distance max_distance = 0;
+                for (const auto& [other_id, _] : graph) {
+                    if (id != other_id) {
+                        try {
+                            auto path = shortest_path(id, other_id);
+                            Distance total_distance = 0;
+                            for (const auto& edge : path) {
+                                total_distance += edge.distance;
+                            }
+                            max_distance = max(max_distance, total_distance);
+                        }
+                        catch (const runtime_error&) {
+                            max_distance = numeric_limits<Distance>::infinity();
+                        }
+                    }
+                }
+                if (max_distance < min_max_distance) {
+                    min_max_distance = max_distance;
+                    best_id = id;
+                }
+            }
+            return best_id;
+        }
     };
-
-    struct Vertex {
-        V _id; 
-        Ver value;
-        vector<Edge> edges; 
-    };
-
-    unordered_map<V, Vertex> graph;
-
-public:
-    bool has_vertex(V id) const {
-        return graph.find(id) != graph.end();
-    }
-    void add_vertex(V id, const Ver& value) {
-        if (!has_vertex(id)) {
-            graph[id] = Vertex{ id, value, {} };
-        }
-    }
-    bool remove_vertex(V id) {
-        auto it = graph.find(id);
-        if (it == graph.end()) return false;
-        graph.erase(it);
-        for (auto& [_, vertex] : graph) {
-            vertex.edges.erase(
-                remove_if(vertex.edges.begin(), vertex.edges.end(),
-                    [id](const Edge& e) { return e._id == id; }),
-                vertex.edges.end());
-        }
-        return true;
-    }
-    vector<V> vertices() const {
-        vector<V> result;
-        for (const auto& [id, _] : graph) {
-            result.push_back(id);
-        }
-        return result;
-    }
-    void add_edge(V from_id, V to_id, const Distance& d) {
-        if (has_vertex(from_id) && has_vertex(to_id)) {
-            graph[from_id].edges.push_back(Edge{ d, to_id });
-        }
-    }
-    bool remove_edge(V from_id, V to_id) {
-        auto it = graph.find(from_id);
-        if (it == graph.end()) return false;
-
-        auto& edges = it->second.edges;
-        auto edge_it = remove_if(edges.begin(), edges.end(),
-            [to_id](const Edge& e) { return e._id == to_id; });
-        if (edge_it == edges.end()) return false;
-
-        edges.erase(edge_it, edges.end());
-        return true;
-    }
-    bool remove_edge(const Edge& e, V from_id) {
-        return remove_edge(from_id, e._id);
-    }
-    bool has_edge(V from_id, V to_id) const {
-        auto it = graph.find(from_id);
-        if (it == graph.end()) return false;
-
-        const auto& edges = it->second.edges;
-        return any_of(edges.begin(), edges.end(),
-            [to_id](const Edge& e) { return e._id == to_id; });
-    }
-    bool has_edge(const Edge& e, V from_id) const {
-        return has_edge(from_id, e._id);
-    }
-};
+}
